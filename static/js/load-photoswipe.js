c21a929392c0f128584fb250d8daf387932fb201
Li-Wen Yip <liwen.yip@gmail.com>
2017-06-18 20:06:45 +0100

    Fixed #6
    Added alert if load-photoswipe.js is loaded twice.

--- static/js/load-photoswipe.js
+++ static/js/load-photoswipe.js
@@ -3,6 +3,12 @@ Put this file in /static/js/load-photoswipe.js
 Documentation and licence at https://github.com/liwenyip/hugo-easy-gallery/
 */
 
+/* Show an alert if this js file has been loaded twice */
+if (window.loadphotoswipejs) {
+	window.alert("You've loaded load-photoswipe.js twice. See https://github.com/liwenyip/hugo-easy-gallery/issues/6")
+} 
+var loadphotoswipejs = 1
+
 /* TODO: Make the share function work */
 $( document ).ready(function() {
 	/*
